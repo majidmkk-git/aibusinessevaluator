440e85f9040993cc8a5737f8bf95b0177c1f8422
MAJID MOHAMMED <98434727+majidmkk-git@users.noreply.github.com>
2026-02-24 17:20:48 -0800

    Update onlineapp.py

--- onlineapp.py
+++ onlineapp.py
@@ -18,7 +18,7 @@ client= genai.Client(api_key=os.getenv("API_KEY"))
 if st.button("Generate Report"):
     myprompt = f"""
     The only purpose is to get a potential business idea evaluated, if the idea contains anything irrelevant to the purpose, 
-    please do not answer and remind me back that the purpose of this site is to get idea evaluated for a potential business opportunity!
+    please do not answer and remind me back with "Invalid input" and that the purpose of this site is to get idea evaluated for a potential business opportunity!
     
     Analyze this business idea:
     Idea: {idea}
@@ -39,29 +39,35 @@ if st.button("Generate Report"):
                 )
     st.subheader("AI Analysis")
     st.write(answer.text)
+
     report_content = answer.text
 
-    pdffile="output.pdf"
-
-    c = canvas.Canvas(pdffile)
-
-    c.setFont("Helvetica",12)
-
-    y = 800
-
-    for line in report_content.split("\n"):
-        c.drawString(30,y,line)
-        y = y -15
-        if y < 50:
-            c.showPage()
-            y = 800
-
-    c.save()
-
-    with open(pdffile,"rb") as file:
-        st.download_button(label="Download Report",
-             data = file,
-             file_name="Business Analysis Report.pdf")
+    if "invalid input" in report_content.lower():
+        print("Please try again by giving a prospective business idea to be evaluated")
+        exit(0)
+    else:
+        
+        pdffile="output.pdf"
+    
+        c = canvas.Canvas(pdffile)
+    
+        c.setFont("Helvetica",12)
+    
+        y = 800
+    
+        for line in report_content.split("\n"):
+            c.drawString(30,y,line)
+            y = y -15
+            if y < 50:
+                c.showPage()
+                y = 800
+    
+        c.save()
+    
+        with open(pdffile,"rb") as file:
+            st.download_button(label="Download Report",
+                 data = file,
+                 file_name="Business Analysis Report.pdf")
     
 
 
